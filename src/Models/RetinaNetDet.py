6e3181314b837935ead1406579afa77333376b47
Alex <luigi.taglialatela1996@gmail.com>
2020-10-20 21:03:05 +0200

    changing initial epoch

--- src/Models/RetinaNetDet.py
+++ src/Models/RetinaNetDet.py
@@ -33,7 +33,7 @@ class RetinaNetDet(implements(IModelService)):
                                                                 valSize = config["valSize"])
     
     def Train(self):
-        os.system("../keras-retinanet/keras_retinanet/bin/train.py --tensorboard-dir ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput --snapshot-path ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput/Snapshots --snapshot ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput/Snapshots/resnet50_pascal_06.h5 --random-transform --steps 100 pascal ../LogosVOC --no-evaluation")
+        os.system("../keras-retinanet/keras_retinanet/bin/train.py --tensorboard-dir ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput --snapshot-path ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput/Snapshots --snapshot ../../content/drive/My\ Drive/SingleDetectionUtility/TrainingOutput/Snapshots/resnet50_pascal_03.h5 --random-transform --steps 100 pascal ../LogosVOC --no-evaluation --initial-epoch 9")
     
     def Predict(self):
         print("testing retinanet")
